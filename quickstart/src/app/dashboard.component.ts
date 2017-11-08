d10600dc322609af652a2fb2556971a02ec1c2da
Borchani yessine <borchaniz96@gmail.com>
2017-11-08 22:09:03 +0100

    Done with the Hero-detail-component

--- quickstart/src/app/dashboard.component.ts
+++ quickstart/src/app/dashboard.component.ts
@@ -1,7 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-@Component({
-  selector: 'my-dashboard',
-  template: '<h3>My Dashboard</h3>'
-})
-export class DashboardComponent { }
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
+
+  @Component({
+    selector: 'my-dashboard',
+    templateUrl: './dashboard.component.html',
+  })
+export class DashboardComponent {
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes.slice(1, 5));
+  }
+}
